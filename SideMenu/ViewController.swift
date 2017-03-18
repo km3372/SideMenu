d1ae705ddd84739675baca85c40862f8303ccf7a
Ken Moody <km3372@gmail.com>
2017-03-18 08:11:23 -0400

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Ken Moody on 3/18/17.
+//  Copyright © 2017 Ken Moody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
